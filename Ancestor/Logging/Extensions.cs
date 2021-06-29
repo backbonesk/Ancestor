a74359ba226724ee5ba8525aba768db89308ba81
Michal Záhradník <michal.zahradnik@backbone.sk>
2021-06-29 10:54:06 +0200

    Logging Extensions.cs

---
+++ Ancestor/Logging/Extensions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Logging;
+using Sentry.Protocol;
+
+namespace Ancestor.Logging
+{
+    public static class Extensions
+    {
+        public static SentryLevel ToSentryLevel(this LogLevel logLevel)
+        {
+            SentryLevel sentryLogLevel;
+            
+            switch (logLevel)
+            {
+                case LogLevel.None:
+                    sentryLogLevel = SentryLevel.Info;
+                    break;
+                case LogLevel.Information:
+                    sentryLogLevel = SentryLevel.Info;
+                    break;
+                case LogLevel.Trace:
+                    sentryLogLevel = SentryLevel.Info;
+                    break;
+                case LogLevel.Debug:
+                    sentryLogLevel = SentryLevel.Debug;
+                    break;
+                case LogLevel.Warning:
+                    sentryLogLevel = SentryLevel.Warning;
+                    break;
+                case LogLevel.Error:
+                    sentryLogLevel = SentryLevel.Error;
+                    break;
+                case LogLevel.Critical:
+                    sentryLogLevel = SentryLevel.Fatal;
+                    break;
+                default:
+                    sentryLogLevel = SentryLevel.Info;
+                    break;
+            }
+
+            return sentryLogLevel;
+        }
+    }
+}
